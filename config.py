d7d0ea763699ced4b4df69ecfa71a9cb9faefa8e
martroben <87522742+martroben@users.noreply.github.com>
2022-12-14 00:00:09 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+
+STORAGE_DIR: str = "/apartment_bot"
+LOG_FOLDER: str = "/media/mart/Data/martroben/Projects/Python/apartment_bot/apartment_bot_py/log"
+# f"{STORAGE_DIR}/log"
+TOR_TORRC_PATH: str = "/etc/tor/torrc"
+TOR_PROCESS_NAME: str = "tor"
+TOR_CONTROL_PORT_PASSWORD: str               # Set by tor_operations.setup_tor() function
+CHROME_VERSION: str = "108"
+ACTIVE_LISTINGS_FILENAME: str = "active_listings.csv"
+EXPIRED_LISTINGS_FILENAME: str = "expired_listings.csv"
+MAX_REQUEST_ARCHIVE_SIZE_MB: float = 5
+C24_INDICATOR: str = "c24"
+KV_INDICATOR: str = "kv"
+REQUESTS_ARCHIVE_DIR: str = "requests_archive"
+KV_BASE_URL: str = "https://www2.kv.ee"
+C24_BASE_URL: str = "https://m-api.city24.ee/et_EE/search/realties"
+
+# Gives error 451 (rejected for legal reasons) when using python requests module's default header.
+USER_AGENT_HEADER: str = "curl/7.81.0"
