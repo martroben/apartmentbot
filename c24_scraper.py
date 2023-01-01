9029304b8fff8091fdb110c11bb4acf156f7fcb4
martroben <mart.roben@gmail.com>
2023-01-01 03:18:52 +0200

    Working version of c24 scraper

--- c24_scraper.py
+++ c24_scraper.py
@@ -1,23 +1,104 @@
-import requests
-from requests import Request, Session
+from requests import Request
 import random
 import os
-
-C24_BASE_URL = "https://m-api.city24.ee/et_EE/search/realties"
-CONFIG_DIR_PATH = "/home/mart/Python/apartmentbot/config"
-USER_AGENTS_FILE = "user_agents"
-TOR_HOST = "127.0.0.1"
-SOCKS_PORT = 9050
-# Areas:
-# 3166 - Põhja-Tallinn
-# 1535 - Kristiine
-C24_AREAS = "1535,3166"
-C24_N_ROOMS = "3,4,5,6"
+import logging
+from datetime import datetime
+from time import sleep
+from urllib.error import ContentTooShortError
+import undetected_chromedriver as uc
 
 
-def get_c24_request(n_rooms: str, areas: str) -> requests.Request:
+os.environ["C24_BASE_URL"] = "https://m-api.city24.ee/et_EE/search/realties"
+os.environ["CONFIG_DIR_PATH"] = "/home/mart/Python/apartmentbot/config"
+os.environ["LOG_DIR_PATH"] = "/home/mart/Python/apartmentbot/log"
+os.environ["SCRAPED_PAGES_NEW_PATH"] = "/home/mart/Python/apartmentbot/log/scraped_pages/new"
+os.environ["USER_AGENTS_FILE"] = "user_agents"
+os.environ["TOR_HOST"] = "127.0.0.1"
+os.environ["SOCKS_PORT"] = "9050"
+os.environ["C24_AREAS"] = "3166"  # 3166 - Põhja-Tallinn, 1535 - Kristiine
+os.environ["C24_N_ROOMS"] = "3"
+os.environ["C24_INDICATOR"] = "c24"
+
+
+def get_human_wait_time():
     """
-    Query c24 for apartments.
+    Get a "human-like" wait time (for navigating to new page etc.)
+    :return: Values between 5 and 20.
+    """
+    average_wait_time_sec = 3
+    wait_time = random.expovariate(lambd=1/average_wait_time_sec)
+    if wait_time < 3 or wait_time > 15:
+        wait_time = get_human_wait_time()
+    logging.info(f"Inserting wait time: {round(wait_time,2)} seconds.")
+    return wait_time
+
+
+def retry_function(times: int = 3, exceptions=Exception, retry_interval_sec: int = random.randint(3, 10)):
+    """
+    Retries the wrapped function. Meant to be used as a decorator.
+
+    Optional parameters:
+    times: int - The number of times to repeat the wrapped function (default: 3).
+    exceptions: list[Exception] - List of exceptions that trigger a retry attempt (default: Exception).
+    retry_interval_sec: int - How many seconds to wait between retry attempts (default: random integer between 3 and 10)
+    """
+    def decorator(function):
+        def inner_function(*args, **kwargs):
+            attempt = 1
+            while attempt <= times:
+
+                try:
+                    return function(*args, **kwargs)
+                except exceptions as exception:
+                    log_string = f"Retrying function {function.__name__} in {retry_interval_sec} seconds, " \
+                                 f"because {type(exception).__name__} exception occurred: {exception}\n" \
+                                 f"Attempt {attempt} of {times}."
+                    logging.exception(log_string)
+                    sleep(retry_interval_sec)
+                    attempt += 1
+        return inner_function
+    return decorator
+
+
+@retry_function(exceptions=ContentTooShortError)
+def get_chrome_driver(options: uc.ChromeOptions = uc.ChromeOptions(),
+                      chrome_driver_log_path: str =
+                      os.path.join(os.environ["LOG_DIR_PATH"], "chromedriver.log")) -> uc.Chrome:
+    """
+    Start an uc chrome driver.
+
+    :param options: chrome driver options object.
+    :param chrome_driver_log_path: chrome driver.
+    :return: a chrome driver object.
+    """
+    if not os.path.isdir(os.path.dirname(chrome_driver_log_path)):
+        warning_string = f"Directory for logfile {chrome_driver_log_path} doesn't exist. " \
+                         f"This may cause Chrome driver to quit unexpectedly."
+        raise UserWarning(warning_string)
+    driver = uc.Chrome(options=options, service_log_path=chrome_driver_log_path)
+    driver.set_page_load_timeout(120)
+    return driver
+
+
+@retry_function(retry_interval_sec=get_human_wait_time())
+def uc_scrape_page(url: str, driver: uc.Chrome) -> str:
+    """
+    Scrape the target url with uc.
+
+    :param url: Target url.
+    :param driver: Chromedriver object.
+    :return: Page source string.
+    """
+    driver.get(url)
+    sleep(get_human_wait_time())
+    scraped_data = driver.page_source
+    driver.close()
+    return scraped_data
+
+
+def get_c24_request(n_rooms: str, areas: str) -> Request:
+    """
+    Get a request for c24.
 
     Example get request from inspection:
     https://m-api.city24.ee/et_EE/search/realties?address[cc]=1&address[city][]=3166&address[city][]=1535&tsType=sale&unitType=Apartment&roomCount=3&itemsPerPage=500&page=1
@@ -28,10 +109,10 @@ def get_c24_request(n_rooms: str, areas: str) -> requests.Request:
     Can add several address[city] parameters to query several areas.
     To query several room counts, use roomCount=3,4,5+
 
-    :param n_rooms: Number of rooms to query for. Either int or list of ints.
+    :param n_rooms: Number of rooms to query for. String with comma separated values.
+    :param areas: Area codes to query. String with comma separated values.
     :return: requests.models.Response object.
     """
-    c24_headers = {"User-Agent": USER_AGENT}
     c24_parameters = {
         "tsType": "sale",
         "unitType": "Apartment",
@@ -50,22 +131,93 @@ def get_c24_request(n_rooms: str, areas: str) -> requests.Request:
     else:
         c24_parameters["roomCount"] = ",".join([str(i) for i in n_rooms])
 
-    request = Request("GET", C24_BASE_URL, params=c24_parameters, headers=c24_headers)
+    request = Request("GET", C24_BASE_URL, params=c24_parameters)
     return request
 
 
-# Get a random user agent
-user_agents = list()
-with open(os.path.join(CONFIG_DIR_PATH, USER_AGENTS_FILE), "r") as user_agents_file:
-    for line in user_agents_file:
-        user_agents += [line.strip()]
-USER_AGENT = random.choice(user_agents)
+if __name__ == "__main__":
 
-tor_proxies = {
-   "http": f"{TOR_HOST}:{SOCKS_PORT}",
-   "https": f"{TOR_HOST}:{SOCKS_PORT}"}
+    # Set logging
+    LOG_DIR_PATH = os.environ["LOG_DIR_PATH"]
+    if not os.path.exists(LOG_DIR_PATH):
+        os.makedirs(LOG_DIR_PATH)
 
-c24_request = get_c24_request(n_rooms=C24_N_ROOMS, areas=C24_AREAS)
+    logging.basicConfig(
+        filename=f"{LOG_DIR_PATH}/{datetime.today().strftime('%Y_%m')}.log",
+        format="{asctime}|{funcName}|{levelname}:{message}",
+        style="{",
+        level=logging.INFO)
 
-# c24_session = Session()
-# c24_session.send(c24_request.prepare(), proxies=tor_proxies)
+    logging.info("c24 scraper started")
+
+    # Randomize scrape times
+    # if random.choice(list(range(3))) == 1:
+    #     logging.info("c24 scraper exited with no action (randomization)")
+    #     exit()
+    # random_sleep_time = random.uniform(0, 3500)
+    # logging.info(f"c24 scraper sleeping for {round(random_sleep_time/60, 2)} minutes before action (randomization)")
+    # sleep(random_sleep_time)
+
+    # Load environmental variables
+    try:
+        C24_BASE_URL = os.environ["24_BASE_URL"]
+        CONFIG_DIR_PATH = os.environ["CONFIG_DIR_PATH"]
+        SCRAPED_PAGES_NEW_PATH = os.environ["SCRAPED_PAGES_NEW_PATH"]
+        USER_AGENTS_FILE = os.environ["USER_AGENTS_FILE"]
+        TOR_HOST = os.environ["TOR_HOST"]
+        SOCKS_PORT = os.environ["SOCKS_PORT"]
+        C24_AREAS = os.environ["C24_AREAS"]
+        C24_N_ROOMS = os.environ["C24_N_ROOMS"]
+        C24_INDICATOR = os.environ["C24_INDICATOR"]
+    except KeyError as error:
+        log_string = f"While load environmental variables " \
+                     f"{type(error).__name__} occurred: {error}"
+        logging.exception(log_string)
+        exit(1)
+
+    # Specify tor connection
+    # Socket format: https://devpress.csdn.net/python/62fe30f8c6770329308047f0.html
+    socks_socket = f"socks5://{TOR_HOST}:{SOCKS_PORT}"
+
+    # Get request url
+    c24_request = get_c24_request(n_rooms=C24_N_ROOMS, areas=C24_AREAS)
+    c24_request_url = c24_request.prepare().url
+
+    # Start chrome_driver
+    try:
+        chrome_options = uc.ChromeOptions()
+        chrome_options.add_argument(f"--proxy-server={socks_socket}")
+        chrome_driver = get_chrome_driver(
+            options=chrome_options,
+            chrome_driver_log_path=os.path.join(LOG_DIR_PATH, "chromedriver.log"))
+    except Exception as exception:
+        log_string = f"While loading Chrome driver " \
+                     f"{type(exception).__name__} occurred: {exception}"
+        logging.exception(log_string)
+        exit(1)
+
+    # Do the scraping
+    logging.info(f"Executing c24 scraping with url {c24_request_url}")
+    try:
+        c24_page = uc_scrape_page(c24_request_url, chrome_driver)
+    except Exception as exception:
+        log_string = f"While trying to scrape c24, " \
+                     f"{type(exception).__name__} occurred: {exception}"
+        logging.exception(log_string)
+        del log_string
+        c24_page = ""
+
+    # Export results
+    if c24_page:
+        if not os.path.exists(SCRAPED_PAGES_NEW_PATH):
+            os.makedirs(SCRAPED_PAGES_NEW_PATH)
+
+        c24_export_file_name = f"{datetime.today().strftime('%Y_%m_%d_%H%M')}_{C24_INDICATOR}"
+        with open(os.path.join(SCRAPED_PAGES_NEW_PATH, c24_export_file_name), "w", encoding="UTF-8") as c24_export_file:
+            c24_export_file.write(c24_page)
+    else:
+        log_string = "c24 scrape session unsuccessful!"
+        logging.error(log_string)
+        del log_string
+
+    logging.info("c24 scraper finished")
